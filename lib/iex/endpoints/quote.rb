c42679a008df793bbbf4a6b244255c12f592d753
blackchestnut <alexander.kalinichev@gmail.com>
2020-07-19 10:24:45 +0300

    Add quote_field method

--- lib/iex/endpoints/quote.rb
+++ lib/iex/endpoints/quote.rb
@@ -1,8 +1,26 @@
+# frozen_string_literal: true
+
 module IEX
   module Endpoints
     module Quote
+      # https://iexcloud.io/docs/api/#quote
       def quote(symbol, options = {})
-        IEX::Resources::Quote.new(get("stock/#{symbol}/quote", { token: publishable_token }.merge(options)))
+        IEX::Resources::Quote.new(
+          get(
+            "stock/#{symbol}/quote",
+            { token: publishable_token }.merge(options)
+          )
+        )
+      rescue Faraday::ResourceNotFound => e
+        raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
+      end
+
+      # https://iexcloud.io/docs/api/#quote
+      def quote_field(symbol, field, options = {})
+        get(
+          "stock/#{symbol}/quote/#{field}",
+          { token: publishable_token }.merge(options)
+        )
       rescue Faraday::ResourceNotFound => e
         raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
       end
